4f40831fdd7fdcdd48b7cc62e18c05866ec5d5d4
asonas <hzw1258@gmail.com>
2016-01-24 01:29:16 +0900

    Update generators

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module Rails5base
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.helper false
+
+      g.fixture_replacement :fabrication
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
+    end
   end
 end
